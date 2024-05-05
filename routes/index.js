b5dd4779d7a613728ae3699a4aa4844ce1591801
Prem Trivedi <premtrivedi155@gmail.com>
2024-05-05 13:34:11 -0400

    wrooongg

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require('./users');
-const postModel = require('./post');
+const userModel = require('./users')
 const passport = require('passport');
 const localStrategy = require('passport-local');
-const upload = require('./multer');
 
 passport.use(new localStrategy(userModel.authenticate()));
 
 
 router.get('/', function(req, res, next) {
-  res.render('index',{nav:false});
+  res.render('index');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register',{nav:false});
+  res.render('register');
 });
 
 router.get('/profile', isLoggedIn, async function(req, res, next) {
@@ -69,13 +67,13 @@ router.post('/register', function(req, res, next) {
   })
   userModel.register(data, req.body.password)
   .then(function(){
-    passport.authenticate("local")(req,res,function(){
+    passport.authenticate("local"),(req,res,function(){
       res.redirect("/profile");
     })
   })
 });
 
-router.post('/login',passport.authenticate("local",{
+router.get('/login',passport.authenticate("local",{
   failureRedirect:"/",
   successRedirect: "/profile",
 }), function(req,res,next){
